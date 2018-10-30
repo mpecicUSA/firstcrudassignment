f54958524fb98aa23a374608a8e971b7e555d5b9
Marko Pecic <mpecic2010@gmail.com>
2018-10-29 17:25:21 -0700

    CRUD APP CREATED

--- server.js
+++ server.js
@@ -3,24 +3,9 @@ const express = require("express");
 const app = express();
 var port = process.env.PORT || 8000;
 let id = 0;
-let dataAsArr = []
+let dataAsArr = [];
+let promArr = [];
 // Here's a list of all the routes you'll need.
-
-//  - Create route for creating new users // WORKING !!
-
-app.post("/create/:name/:email/:state", function(req,res){
-  console.log("Does this work");
-  let newObj = {
-    name: req.params.name,
-    email: req.params.email,
-    state: req.params.state
-  }
-  console.log(newObj);
-  dataAsArr = JSON.parse(fs.readFileSync("./storage.json"));
-  dataAsArr.push(newObj);
-  let data2 = (JSON.stringify(dataAsArr));
-  fs.writeFileSync("./storage.json", data2 );
-});
 
 //  - Get route for getting all users// this works YAY!
 app.get('/', function(req,res){
@@ -29,42 +14,66 @@ app.get('/', function(req,res){
   })
 });
 
-// //  - Get route for getting a user by name
-// app.get("/:name", function(req,res){
-//   fs.readFile("./storage.json", "utf8", function(err,data){
-//     let someData = JSON.parse(data);
-//     let matchedUser = someData.filter((item)=> {
-//       return item.name  == req.params.name;
-//     });
-//
-//     if(matchedUser.length>=1){
-//       res.json(matchedUser[0]);
-//     }else{
-//       res.sendStatus(400);
-//     }
-//   })
-// })
+// //  - Get route for getting a user by name// works
+app.get("/:name", function(req,res){
+  fs.readFile("./storage.json", "utf8", function(err,data){
+    let someData = JSON.parse(data);
+    let matchedUser = someData.filter((item)=> {
+      return item.name  == req.params.name;
+    });
 
-// //  - Update route for updating a user by name
+    if(matchedUser.length>=1){
+      res.json(matchedUser[0]);
+    }else{
+      res.sendStatus(400);
+    }
+  })
+})
+//  - Create route for creating new users // WORKING !!
 
-// app.patch("/:user/:email/:state", function(req, res){
-//   fs.readFile("./storage.json", "utf8", function(err,data){
-//     let someData = JSON.parse(data);
-//     let matchedUser = someData.filter((item)=> {
-//       return item.name  == req.params.name;
-//     });
-//
-//     if(matchedUser.length>=1){
-//
-//     }else{
-//       res.sendStatus(400);
-//     }
-//   })
-// })
-//  - Delete route for deleting a user by name
+app.post("/create/:name/:email/:state", function(req,res){
+  res.send(`A user has been added`)
+  let newObj = {
+    name: req.params.name,
+    email: req.params.email,
+    state: req.params.state
+  }
+  dataAsArr = JSON.parse(fs.readFileSync("./storage.json"));
+  dataAsArr.push(newObj);
+  let data2 = (JSON.stringify(dataAsArr));
+  fs.writeFileSync("./storage.json", data2 );
+});
+// //  - Update route for updating a user by name // ??
+app.patch('/:name/:email/:state', (req, res)=>{
+  res.send("Your update has been submitted");
+  let priorStorage = JSON.parse(fs.readFileSync("./storage.json"));
+  for(let i = 0; i < priorStorage.length; i++ ){
+    if (req.params.name === priorStorage[i].name){
+      priorStorage[i].email = req.params.email
+      priorStorage[i].state = req.params.state;
+    }
+  };
+  fs.writeFileSync("./storage.json", JSON.stringify(priorStorage));
+});
+//  - Delete route for deleting a user by name // Works!
+app.delete('/:name', function(req, res) {
+  let name = req.params.name;
+  fs.readFile("./storage.json", "utf8", function(err,data){
+    let dataBase = JSON.parse(data);
+    let results = dataBase.filter((item)=> {
+      return item.name  !== req.params.name;
+    });
 
-// app.delete("/:user",)
-
+    if(results.length < dataBase.length){
+      let data2 = (JSON.stringify(results));
+      fs.writeFileSync("./storage.json", data2 );
+      res.send(`${name} has been removed`)
+    }else{
+      res.send("user not found")
+      res.sendStatus(404);
+    }
+  })
+});
 app.listen(port, function() {
   console.log('Listening on port', port);
 });
